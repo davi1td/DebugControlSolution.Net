63bd26f199a27ae4e861a0eda81be47c2c694c46
tdavis <tdavis@WIN10HOME>
2021-05-31 11:17:45 -0400

    Add project files.

---
+++ .net 4.8/DebugControl4.8/DebugControl.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace DebugControlLibrary
+{
+    public partial class DebugControl: UserControl
+    {
+        public DebugControl()
+        {
+            InitializeComponent();
+        }
+        private string demoStringValue = null;
+        [Browsable(true)]
+        public string DemoString
+        {
+            get
+            {
+                return this.demoStringValue;
+            }
+            set
+            {
+                demoStringValue = value;
+            }
+        }
+    }
+}
---
+++ .net Core/DebuggingExample/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+using DebugControlLibrary;
+
+namespace DebuggingExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.debugControl1 = new DebugControlLibrary.DebugControl();
+            this.SuspendLayout();
+            // 
+            // debugControl1
+            // 
+            this.debugControl1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.debugControl1.DemoString = "";
+            this.debugControl1.Location = new System.Drawing.Point(45, 24);
+            this.debugControl1.Name = "debugControl1";
+            this.debugControl1.Size = new System.Drawing.Size(559, 429);
+            this.debugControl1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(714, 574);
+            this.Controls.Add(this.debugControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DebugControl debugControl1;
+    }
+}
+
